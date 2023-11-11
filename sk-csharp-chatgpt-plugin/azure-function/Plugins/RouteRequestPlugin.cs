c5c9e04f2be85b8ecebb927eed35a535e8a31e0e
Daniel Ciborowski <dciborow@microsoft.com>
2023-11-11 05:29:13 -0500

    Update RouteRequestPlugin.cs

--- sk-csharp-chatgpt-plugin/azure-function/Plugins/RouteRequestPlugin.cs
+++ sk-csharp-chatgpt-plugin/azure-function/Plugins/RouteRequestPlugin.cs
@@ -11,19 +11,49 @@ public class RouteRequestPlugin
     )
     {
         // Save the original user request
-        // var sampleRequest = "If my investment of 2130.23 dollars increased by 23%, how much would I have after I spent $5 on a latte?";
-        var request = input;
-        var adoBuildPlugin = kernel.ImportFunctions(new Plugins.AzureDevOpsPlugin.AzureDevOpsBuildPlugin(), "AzureDevOpsBuildPlugin");
+        string request = input;
+    
+        // Retrieve the intent from the user request
+        var getIntent = _kernel.Functions.GetFunction("OrchestratorPlugin", "GetIntent");
+        var getIntentVariables = new ContextVariables
+        {
+            ["input"] = input,
+            ["options"] = "Code, PullRequest, Build, Release"
+        };
+        string intent = (await _kernel.RunAsync(getIntentVariables, getIntent)).GetValue<string>()!.Trim();
+    
+        // Call the appropriate function
+        IAzureDevOpsPlugin AzureDevOpsPlugin;
+        switch (intent)
+        {
+            case "Code":
+                AzureDevOpsPlugin =  _kernel.ImportFunctions(new Plugins.AzureDevOpsPlugin.AzureDevOpsReleasePlugin(), "AzureDevOpsCodePlugin");
+                break;
+            case "PullRequest":
+                AzureDevOpsPlugin =  _kernel.ImportFunctions(new Plugins.AzureDevOpsPlugin.AzureDevOpsReleasePlugin(), "AzureDevOpsPullRequestPlugin");
+                break;
+            case "Build":
+                AzureDevOpsPlugin = _kernel.ImportFunctions(new Plugins.AzureDevOpsPlugin.AzureDevOpsBuildPlugin(), "AzureDevOpsBuildPlugin");
+                break;
+            case "Release":
+                AzureDevOpsPlugin =  _kernel.ImportFunctions(new Plugins.AzureDevOpsPlugin.AzureDevOpsReleasePlugin(), "AzureDevOpsReleasePlugin");
+                break;
+            default:
+                AzureDevOpsPlugin = _kernel.ImportFunctions(new Plugins.CommonPlugin.Common(), "GenerateNewPlugin");
+        }
+        
+        // Use Plugin to Load Optimal Plan if it it exists or create a new one.
+        LoadPlanPlugin =  _kernel.ImportFunctions(new Plugins.CommonPlugin.Common(), "LoadPlanPlugin");
 
         // Create planner
         var planner = new SequentialPlanner(_kernel);
         var plan = await planner.CreatePlanAsync(request);
-        
+        // Run the pipeline
+        var output = await this._kernel.RunAsync(plan);
+
         Console.WriteLine("Plan:\n");
         Console.WriteLine(JsonSerializer.Serialize(plan, new JsonSerializerOptions { WriteIndented = true }));
 
-        // Run the pipeline
-        var output = await this._kernel.RunAsync(plan);
         Console.WriteLine("Plan results:");
         Console.WriteLine(output.GetValue<string>()!.Trim());
 
