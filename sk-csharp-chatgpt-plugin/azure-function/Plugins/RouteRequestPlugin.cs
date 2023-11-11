2514791ae12977c42bbc87eca9159a0f03239d0b
Daniel Ciborowski <dciborow@microsoft.com>
2023-11-11 05:05:37 -0500

    Update RouteRequestPlugin.cs

--- sk-csharp-chatgpt-plugin/azure-function/Plugins/RouteRequestPlugin.cs
+++ sk-csharp-chatgpt-plugin/azure-function/Plugins/RouteRequestPlugin.cs
@@ -54,12 +54,23 @@ public class RouteRequestPlugin
             default:
                 return "I'm sorry, I don't understand.";
         }
-    
+
+        // Add the math plugin
+        var mathPlugin = kernel.ImportFunctions(new Plugins.MathPlugin.Math(), "MathPlugin");
+        
+        // Create planner
+        var planner = new SequentialPlanner(_kernel);
+        var ask = "If my investment of 2130.23 dollars increased by 23%, how much would I have after I spent $5 on a latte?";
+        var plan = await planner.CreatePlanAsync(ask);
+        
+        Console.WriteLine("Plan:\n");
+        Console.WriteLine(JsonSerializer.Serialize(plan, new JsonSerializerOptions { WriteIndented = true }));
+
         // Get remaining functions
         var createResponse = this._kernel.Functions.GetFunction("OrchestratorPlugin", "CreateResponse");
         var getNumbers = this._kernel.Functions.GetFunction("OrchestratorPlugin", "GetNumbers");
         var extractNumbersFromJson = this._kernel.Functions.GetFunction("OrchestratorPlugin", "ExtractNumbersFromJson");
-    
+
         // Run the pipeline
         var output = await this._kernel.RunAsync(
             request,
@@ -68,6 +79,9 @@ public class RouteRequestPlugin
             MathFunction,
             createResponse
         );
+        Console.WriteLine("Plan results:");
+        Console.WriteLine(output.GetValue<string>()!.Trim());
+
         return output.GetValue<string>()!;
     }
 }
